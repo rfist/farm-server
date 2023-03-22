38995c02476c8feb4fa4f8cd6e24d7299b9ce8eb
rfist <theseuship@gmail.com>
2023-03-22 15:53:35 +0100

    add authenticate middleware

---
+++ src/middlewares/authenticate-jwt.middleware.ts
@@ -0,0 +1,22 @@
+import { verify } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import config from '../config/config';
+
+export function authenticateJWT(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    verify(token, config.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+}
